782424d3606612827f3f7aed93dddabceb2cd837
breno <breno@DESKTOP-U63HCPD>
2019-06-30 11:56:09 -0300

    Configuração inicial da HomeScreen e correção das dependências do firebase, que utiliza o AndroidX.

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,19 @@
 import 'package:flutter/material.dart';
+import 'package:loja_virtual/ui/home_screen.dart';
 
-void main() {
-  runApp(MaterialApp(
-    home: null
-  ));
+void main() => runApp(App());
+
+class App extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutters \' Clothing',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        primaryColor: Color.fromARGB(255, 4, 125, 141),
+      ),
+      debugShowCheckedModeBanner: false,
+      home: HomeScreen(),
+    );
+  }
 }
